6a7682886841b0115d65d6d8344a3a02f3c84889
Manuel Franco Sevilla <manuelf@physics.ucsb.edu>
2015-03-05 18:15:47 -0800

    Added mT and MJ plots for inclusive SUSY talk

--- src/plot_iso_1d.cxx
+++ src/plot_iso_1d.cxx
@@ -1,13 +1,15 @@
 // plot_distribution: Macro that plots variables both lumi weighted and normalized to the same area.
 
 #include <iostream>
+#include <vector>
+
 #include "TChain.h"
 #include "TH1D.h"
 #include "TCanvas.h"
 #include "TLegend.h"
 #include "TLine.h"
-#include "TStyle.h"
 #include "TString.h"
+#include "TColor.h"
 
 #include "styles.hpp"
 #include "utilities.hpp"
@@ -24,7 +26,7 @@ public:
     cuts = icuts; cut = icut; samples = isamples;
     tag = ivarname+"_"+cuts; tag.ReplaceAll("_1",""); tag.ReplaceAll(".",""); 
     tag.ReplaceAll("(",""); tag.ReplaceAll("$","");  tag.ReplaceAll(")",""); 
-    tag.ReplaceAll("[",""); tag.ReplaceAll("]",""); 
+    tag.ReplaceAll("[",""); tag.ReplaceAll("]",""); tag.ReplaceAll("||","_");
     tag.ReplaceAll("/","_"); tag.ReplaceAll("*",""); tag.ReplaceAll("&&","_");
     tag.ReplaceAll(">",""); tag.ReplaceAll("<",""); tag.ReplaceAll("=","");
     tag.ReplaceAll("+",""); 
@@ -40,113 +42,124 @@ public:
 
 class sfeats {
 public:
-  sfeats(TString ifile, TString ilabel, int icolor, int istyle=1, TString icut="1"){
+  sfeats(vector<TString> ifile, TString ilabel, int icolor, int istyle=1, TString icut="1"){
     file = ifile; label = ilabel; cut = icut;
     color = icolor; style = istyle;
+    isSig = ifile[0].Contains("T1tttt");// && ifile.Contains("1200");
+    factor = "1";
+    if(ifile[0].Contains("TTW")) factor = "0.36";
   }
-  TString file, label, cut;
+  vector<TString> file;
+  TString label, cut, factor;
   int color, style;
+  bool isSig;
 };
 
-int main(){
-  styles style("1Dtitle"); style.setDefaultStyle();gStyle->SetPadTickY(1);
+int main(){ 
+  styles style("Paper"); style.setDefaultStyle();
   vector<hfeats> vars;
   TCanvas can;
-  TString luminosity="5", folder="/cms2r0/manuelf/root/small/archive/15-01-08/";
+
+  TColor ucsb_blue(1000, 1/255.,57/255.,166/255.);
+  TColor ucsb_gold(1001, 255/255.,200/255.,47/255);
+   TColor penn_red(1002, 149/255.,0/255.,26/255.);
+  TColor uf_orange(1003, 255/255.,74/255.,0/255.);
+  TColor uo_green(1004, 0/255.,79/255.,39/255.);
+  TColor tcu_purple(1005, 52/255.,42/255.,123/255.);
+  TColor tar_heel_blue(1006, 86/255.,160/255.,211/255.);
+  TColor sig_teal(1007, 96/255.,159/255.,128/255.);
+  TColor sig_gold(1008, 215/255.,162/255.,50/255.);
+  TColor seal_brown(1010, 89/255.,38/255.,11/255.);
+
+  TString folder="archive/15-03-03/skim/";
+  vector<TString> s_tt;
+  s_tt.push_back(folder+"*_TTJet*");
+  vector<TString> s_wjets;
+  s_wjets.push_back(folder+"*WJets*");
+  vector<TString> s_single;
+  s_single.push_back(folder+"*_T*channel*");
+  vector<TString> s_ttv;
+  s_ttv.push_back(folder+"*TTW*");
+  s_ttv.push_back(folder+"*TTZ*");
+  vector<TString> s_other;
+  s_other.push_back(folder+"*QCD_HT*");
+  s_other.push_back(folder+"*_ZJet*");
+  s_other.push_back(folder+"*DY*");
+  s_other.push_back(folder+"*WH_HToBB*");
+  vector<TString> s_t1t;
+  s_t1t.push_back(folder+"*T1tttt*1500_*PU20*");
+  vector<TString> s_t1tc;
+  s_t1tc.push_back(folder+"*T1tttt*1200_*PU20*");
+
   // Reading ntuples
   vector<TChain *> chain;
   vector<sfeats> Samples; 
-  // Base Iso samples
-  Samples.push_back(sfeats(folder+"*T1tttt*1500_*PU20*", "T1tttt truth-matched", 4, 1, "mus_tru_tm&&mus_sigid"));
-  Samples.push_back(sfeats("/cms2r0/manuelf/root/small/archive/15-01-14/*TTJet*", "tt truth-matched", kGreen+2, 1, "mus_tru_tm&&mus_sigid"));
-  Samples.push_back(sfeats("/cms2r0/manuelf/root/small/archive/15-01-14/*TTJet*", "tt non-truth-matched", kRed, 1, "!mus_tru_tm&&mus_sigid"));
-  Samples.push_back(sfeats(folder+"*T1tttt*1500_*PU20*", "T1tttt truth-matched", 4, 1, "els_tru_tm&&els_sigid&&els_ispf"));
-  Samples.push_back(sfeats("/cms2r0/manuelf/root/small/archive/15-01-14/*TTJet*", "tt truth-matched", kGreen+2, 1, "els_tru_tm&&els_sigid&&els_ispf"));
-  Samples.push_back(sfeats("/cms2r0/manuelf/root/small/archive/15-01-14/*TTJet*", "tt non-truth-matched", kRed, 1, "!els_tru_tm&&els_sigid&&els_ispf"));
+  Samples.push_back(sfeats(s_other, "Other", 1001));
+  Samples.push_back(sfeats(s_ttv, "ttV", 1002));
+  Samples.push_back(sfeats(s_single, "Single top", 1005));
+  Samples.push_back(sfeats(s_wjets, "W + jets", 1004));
+  Samples.push_back(sfeats(s_tt, "t#bar{t}, 2 l", 1006,1,"((mc_type&0x0F00)/0x100+(mc_type&0x000F)-(mc_type&0x00F0)/0x10)>=2"));
+  Samples.push_back(sfeats(s_tt, "t#bar{t}, 1 l", 1000,1,"((mc_type&0x0F00)/0x100+(mc_type&0x000F)-(mc_type&0x00F0)/0x10)<=1"));
+  Samples.push_back(sfeats(s_t1t, "T1tttt(1500,100)", 2));
+  Samples.push_back(sfeats(s_t1tc, "T1tttt(1200,800)", 2,2));
 
-  // Non-truth-matched electrons broken down
-  Samples.push_back(sfeats("/cms2r0/manuelf/root/small/archive/15-01-14/*TTJet*", "tt non-prompt e", kRed, 1, "!els_tru_tm&&els_sigid&&els_ispf&&abs(els_tru_id)==11"));
-  Samples.push_back(sfeats("/cms2r0/manuelf/root/small/archive/15-01-14/*TTJet*", "tt fake e with no #tau#rightarrowhad", 4, 1, "!els_tru_tm&&els_sigid&&els_ispf&&abs(els_tru_id)!=11&&((mc_type&0x000F)*0x10<=(mc_type&0x00F0))"));
-  Samples.push_back(sfeats("/cms2r0/manuelf/root/small/archive/15-01-14/*TTJet*", "tt fake e with #tau#rightarrowhad", 28, 1, "!els_tru_tm&&els_sigid&&els_ispf&&abs(els_tru_id)!=11&&((mc_type&0x000F)*0x10>(mc_type&0x00F0))"));
-
-  // Samples for truth-matching (DeltaR)
-  Samples.push_back(sfeats("/cms2r0/manuelf/root/small/out/small_quick_SMS-T1tttt_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola_Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v1_MINIAODSIM_UCSB2299_v77.root", "Reconstructed muons", 2, 1, "mus_sigid"));
-  Samples.push_back(sfeats("/cms2r0/manuelf/root/small/out/small_quick_SMS-T1tttt_2J_mGl-1500_mLSP-100_Tune4C_13TeV-madgraph-tauola_Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v1_MINIAODSIM_UCSB2299_v77.root", "Reconstructed electrons", 4, 1, "els_sigid&&els_ispf"));
-
-
- for(unsigned sam(0); sam < Samples.size(); sam++){
+  for(unsigned sam(0); sam < Samples.size(); sam++){
     chain.push_back(new TChain("tree"));
-    chain[sam]->Add(Samples[sam].file);
+    for(unsigned insam(0); insam < Samples[sam].file.size(); insam++)
+      chain[sam]->Add(Samples[sam].file[insam]);
   }
 
-  vector<int> mus_tt_t1;
-  mus_tt_t1.push_back(0);
-  mus_tt_t1.push_back(1);
-  mus_tt_t1.push_back(2);
+  vector<int> mj_sam_lep;
+  mj_sam_lep.push_back(0);
+  mj_sam_lep.push_back(1);
+  mj_sam_lep.push_back(2);
+  mj_sam_lep.push_back(3);
+  mj_sam_lep.push_back(4);
+  mj_sam_lep.push_back(5);
+  mj_sam_lep.push_back(6);
+  mj_sam_lep.push_back(7);
 
-  vector<int> els_tt_t1;
-  els_tt_t1.push_back(3);
-  els_tt_t1.push_back(4);
-  els_tt_t1.push_back(5);
+  vars.push_back(hfeats("mt",20,0,600, mj_sam_lep, "m_{T} (GeV)","ht>500&&met>250&&nbm>=2&&njets>=6&&(Max$(els_pt*els_sigid*(els_miniso_tr10<0.1))>20||Max$(mus_pt*mus_sigid*(mus_miniso_tr10<0.4))>20)&&(nmus+nels)==1",150));
+  vars.push_back(hfeats("mj",20,0,1600, mj_sam_lep, "M_{J} (GeV)","ht>500&&met>250&&nbm>=2&&njets>=6&&mt>150&&(Max$(els_pt*els_sigid*(els_miniso_tr10<0.1))>20||Max$(mus_pt*mus_sigid*(mus_miniso_tr10<0.4))>20)&&(nmus+nels)==1",600));
 
-  vector<int> els_tt_ntm;
-  els_tt_ntm.push_back(4);
-  els_tt_ntm.push_back(6);
-  els_tt_ntm.push_back(7);
-  els_tt_ntm.push_back(8);
+  TString cuts("ht>750&&met>250&&nbm>=2&&njets>=6&&(nmus+nels)==1");
+  //vars.push_back(hfeats("Max$(fjets_r08_m)",40,0,800, mj_sam_lep, "Max(m_{J}^{R=0.8}) (GeV)",cuts));
+  // vars.push_back(hfeats("Max$(fjets_30_m)", 40,0,800, mj_sam_lep, "Max(m_{J}^{R=1.2}) (GeV)",cuts));
+  cuts ="ht>750&&met>250&&nbl>=2&&njets>=4&&mt>150&&(nmus+nels)==1";
+  //vars.push_back(hfeats("Max$(fjets_r08_m)",40,0,800, mj_sam_lep, "Max(m_{J}^{R=0.8}) (GeV)",cuts));
+  // vars.push_back(hfeats("Max$(fjets_30_m)", 40,0,800, mj_sam_lep, "Max(m_{J}^{R=1.2}) (GeV)",cuts));
 
-  vector<int> mus_t1_dr;
-  mus_t1_dr.push_back(9);
-  vector<int> els_t1_dr;
-  els_t1_dr.push_back(10);
-
-  vars.push_back(hfeats("mus_tru_dr",50,0,0.5, mus_t1_dr, "min#DeltaR(#mu_{reco}, #mu_{true})","mus_pt>20",0.1));
-  vars.push_back(hfeats("els_tru_dr",50,0,0.5, els_t1_dr, "min#DeltaR(e_{reco}, e_{true})","els_pt>20",0.1));
-
-  vars.push_back(hfeats("mus_ptrel_0",40,0,40, mus_tt_t1, "Muon p^{rel}_{T} (GeV)","ht>750&&met>200"));
-  vars.push_back(hfeats("mus_ptrel_0",40,0,40, mus_tt_t1, "Muon p^{rel}_{T} (GeV)","ht>750&&met>200&&mus_reliso_r04>0.3"));
-  vars.push_back(hfeats("mus_ptrel_0",40,0,40, mus_tt_t1, "Muon p^{rel}_{T} (GeV)","ht>750&&met>200&&mus_miniso_tr10>0.3"));
-  vars.push_back(hfeats("mus_pt",50,0,400, mus_tt_t1, "Muon p_{T} (GeV)","ht>750&&met>200"));
-  vars.push_back(hfeats("mus_reliso_r04",40,0,2, mus_tt_t1, "Muon relative isolation (R=0.4)","mus_pt>=20&&ht>750"));
-  vars.push_back(hfeats("min(mus_reliso_r02,mus_miniso_tr10)",40,0,2, mus_tt_t1, "Muon mini isolation (0.05<R<0.2)","mus_pt>=20&&ht>750"));
-
-  vars.push_back(hfeats("els_ptrel_0",40,0,40, els_tt_t1, "Electron p^{rel}_{T} (GeV)","ht>750&&met>200"));
-  vars.push_back(hfeats("els_ptrel_0",40,0,40, els_tt_t1, "Electron p^{rel}_{T} (GeV)","ht>750&&met>200&&els_reliso_r03>0.1"));
-  vars.push_back(hfeats("els_ptrel_0",40,0,40, els_tt_t1, "Electron p^{rel}_{T} (GeV)","ht>750&&met>200&&els_miniso_tr10>0.1"));
-  vars.push_back(hfeats("els_pt",50,0,400, els_tt_t1, "Electron p_{T} (GeV)","ht>750&&met>200"));
-  vars.push_back(hfeats("els_reliso_r03",40,0,2, els_tt_t1, "Electron relative isolation (R=0.3)","els_pt>=20&&ht>750"));
-  vars.push_back(hfeats("min(els_reliso_r02,els_miniso_tr10)",40,0,2, els_tt_t1, "Electron mini isolation (0.05<R<0.2)","els_pt>=20&&ht>750"));
-
-  // Non-truth-matched electrons broken down
-  vars.push_back(hfeats("els_reliso_r03",40,0,2, els_tt_ntm, "Electron relative isolation (R=0.3)","ht>750&&els_pt>=20"));
-  vars.push_back(hfeats("min(els_reliso_r02,els_miniso_tr10)",40,0,2, els_tt_ntm, "Electron mini isolation (0.05<R<0.2)","ht>750&&els_pt>=20"));
+  // vars.push_back(hfeats("mj_r08",40,0,2000, mj_sam_lep, "M{J}^{R=0.8} (GeV)",cuts));
+  // vars.push_back(hfeats("mj_30",40,0,2000, mj_sam_lep, "M{J}^{R=1.2} (GeV)",cuts));
 
 
+  TString luminosity="4";
   float minLog = 0.04, maxLog = 10;
-  double legX = 0.44, legY = 0.89, legSingle = 0.075;
+  double legX = 0.6, legY = 0.89, legSingle = 0.055;
   double legW = 0.12, legH = legSingle*vars[0].samples.size();
   TLegend leg(legX, legY-legH, legX+legW, legY);
-  leg.SetTextSize(0.07); leg.SetFillColor(0); leg.SetFillStyle(0); leg.SetBorderSize(0);
+  leg.SetTextSize(0.052); leg.SetFillColor(0); leg.SetFillStyle(0); leg.SetBorderSize(0);
   leg.SetTextFont(132);
 
   TLine line; line.SetLineColor(28); line.SetLineWidth(4); line.SetLineStyle(2);
   vector< vector<TH1D*> > histo[2];
   vector<TH1D*> varhisto;
   vector<float> nentries;
-  float totentries;
   TString hname, pname, variable, leghisto, totCut, title, ytitle;
   for(unsigned var(0); var<vars.size(); var++){
     cout<<endl;
     // Generating vector of histograms
     title = vars[var].cuts; if(title=="1") title = "";
     title.ReplaceAll("nvmus==1&&nmus==1&&nvels==0","1 #mu");
+    title.ReplaceAll("nvmus10==0&&nvels10==0", "0 leptons");  
+    title.ReplaceAll("(Max$(els_pt*els_sigid*(els_miniso_tr10<0.1))>20||Max$(mus_pt*mus_sigid*(mus_miniso_tr10<0.4))>20)&&(nmus+nels)", "n_{lep}");  title.ReplaceAll("njets30","n_{jets}^{30}"); 
     title.ReplaceAll("els_pt","p^{e}_{T}");title.ReplaceAll("mus_pt","p^{#mu}_{T}");
-    title.ReplaceAll("mus_reliso_r04","RelIso"); title.ReplaceAll("els_reliso_r03","RelIso"); title.ReplaceAll("els_reliso","RelIso");
-    title.ReplaceAll("mus_miniso_tr10","MiniIso"); title.ReplaceAll("els_miniso_tr10","MiniIso"); title.ReplaceAll("els_miniso_tr15","MiniIso");
+    title.ReplaceAll("mus_reliso","RelIso"); title.ReplaceAll("els_reliso","RelIso");
+    title.ReplaceAll("mus_miniso_tr15","MiniIso"); title.ReplaceAll("els_miniso_tr15","MiniIso");
     title.ReplaceAll("njets","n_{jets}");title.ReplaceAll("abs(lep_id)==13&&","");
     title.ReplaceAll(">=", " #geq "); title.ReplaceAll(">", " > "); title.ReplaceAll("&&", ", "); 
     title.ReplaceAll("met", "MET"); title.ReplaceAll("ht", "H_{T}");  title.ReplaceAll("mt", "m_{T}"); 
-    title.ReplaceAll("nleps==1", "1 lepton");  title.ReplaceAll("nbm","n_{b}");
+    title.ReplaceAll("nleps==1", "1 lepton");  title.ReplaceAll("nbm","n_{b}"); title.ReplaceAll("==", " = "); 
     title.ReplaceAll("nbl[1]","n_{b,l}");
     for(unsigned his(0); his < 2; his++){
       varhisto.resize(0);
@@ -160,20 +173,18 @@ int main(){
     //// Plotting lumi-weighted distributions in histo[0], and then area-normalized in histo[1] ///
     leg.Clear();
     nentries.resize(0);
-    totentries = 0;
     variable = vars[var].varname;
     float maxhisto(-999);
     for(unsigned sam(0); sam < vars[var].samples.size(); sam++){
       int isam = vars[var].samples[sam];
-      bool isSig = Samples[isam].file.Contains("T1tttt");
-      isSig=(sam==1);
-      totCut = luminosity+"*weight*("+vars[var].cuts+"&&"+Samples[isam].cut+")"; 
+      bool isSig = Samples[isam].isSig;
+      totCut = Samples[isam].factor+"*"+luminosity+"*weight*("+vars[var].cuts+"&&"+Samples[isam].cut+")"; 
+      //cout<<totCut<<endl;
       chain[isam]->Project(histo[0][var][sam]->GetName(), variable, totCut);
       histo[0][var][sam]->SetBinContent(vars[var].nbins,
 					  histo[0][var][sam]->GetBinContent(vars[var].nbins)+
 					  histo[0][var][sam]->GetBinContent(vars[var].nbins+1));
       nentries.push_back(histo[0][var][sam]->Integral(1,vars[var].nbins));
-      totentries += nentries[sam];
       histo[0][var][sam]->SetXTitle(vars[var].title);
       ytitle = "Entries for "+luminosity+" fb^{-1}";
       if(vars[var].unit!="") {
@@ -189,10 +200,12 @@ int main(){
 
       if(!isSig){ // Adding previous bkg histos
 	for(int bsam(sam-1); bsam >= 0; bsam--){
-	  if(!Samples[vars[var].samples[bsam]].file.Contains("T1tttt")){
-	    histo[0][var][sam]->Add(histo[0][var][bsam]);
-	    break;
-	  }
+	  histo[0][var][sam]->Add(histo[0][var][bsam]);
+	  break;
+	  // if(!Samples[vars[var].samples[bsam]].file[0].Contains("T1tttt")){
+	  //   histo[0][var][sam]->Add(histo[0][var][bsam]);
+	  //   break;
+	  // }
 	}
 	histo[0][var][sam]->SetFillColor(Samples[isam].color);
 	histo[0][var][sam]->SetFillStyle(1001);
@@ -210,8 +223,7 @@ int main(){
       int isam = vars[var].samples[sam];
       leghisto = Samples[isam].label+" [N = " + RoundNumber(nentries[sam],0) + "]";
       leg.AddEntry(histo[0][var][sam], leghisto);
-      bool isSig = Samples[isam].file.Contains("T1tttt");
-      isSig = (sam==1);
+      bool isSig = Samples[isam].isSig;
       if(!isSig){
 	if(firstplotted < 0) {
 	  histo[0][var][sam]->Draw();
@@ -221,8 +233,7 @@ int main(){
     }
     for(int sam(vars[var].samples.size()-1); sam >= 0; sam--){
       int isam = vars[var].samples[sam];
-      bool isSig = Samples[isam].file.Contains("T1tttt");
-      isSig = (sam==1);
+      bool isSig = Samples[isam].isSig;
       if(isSig) histo[0][var][sam]->Draw("same");
     }
     legH = legSingle*vars[var].samples.size(); leg.SetY1NDC(legY-legH);
@@ -236,12 +247,12 @@ int main(){
     }
     if(vars[var].cut>0) line.DrawLine(vars[var].cut, 0, vars[var].cut, maxhisto*maxLog);
     can.SetLogy(1);
-    pname = "eps/log_lumi_"+vars[var].tag+".eps";
+    pname = "plots/1d/log_lumi_"+vars[var].tag+".eps";
     can.SaveAs(pname);
     histo[0][var][firstplotted]->SetMinimum(0);
     histo[0][var][firstplotted]->SetMaximum(maxhisto*1.1);
     can.SetLogy(0);
-    pname = "eps/lumi_"+vars[var].tag+".eps";
+    pname = "plots/1d/lumi_"+vars[var].tag+".eps";
     can.SaveAs(pname);
 
     //////////// Plotting area-normalized distributions ////////////
@@ -258,23 +269,20 @@ int main(){
 	histo[1][var][sam]->SetYTitle("Entries (%)");
 	histo[1][var][sam]->Draw();
       } else histo[1][var][sam]->Draw("same");
-      // leghisto = Samples[isam].label+" ["+RoundNumber(nentries[sam]*100,1,totentries)+"%]";//+"%, #mu = ";
       leghisto = Samples[isam].label+" [#mu = ";
       int digits(1);
       leghisto += RoundNumber(histo[1][var][sam]->GetMean(),digits) + "]";
-      if(vars[var].varname.Contains("tru_dr")) leghisto = Samples[isam].label;
       leg.AddEntry(histo[1][var][sam], leghisto);
     } // Loop over samples
     leg.Draw(); 
     if(vars[var].cut>0) line.DrawLine(vars[var].cut, 0, vars[var].cut, maxhisto*1.1);
     histo[1][var][0]->SetMaximum(maxhisto*1.1);
     can.SetLogy(0);
-    pname = "eps/shapes_"+vars[var].tag+".eps";
+    pname = "plots/1d/shapes_"+vars[var].tag+".eps";
     can.SaveAs(pname);
     histo[1][var][0]->SetMaximum(maxhisto*maxLog);
-    //histo[1][var][0]->SetMinimum(0.01);
     can.SetLogy(1);
-    pname = "eps/log_shapes_"+vars[var].tag+".eps";
+    pname = "plots/1d/log_shapes_"+vars[var].tag+".eps";
     can.SaveAs(pname);
   }// Loop over variables
 
